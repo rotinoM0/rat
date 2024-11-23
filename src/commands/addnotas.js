4a834ede29ced89cbb49048eeb14ce1bf8f54161
rotinoM0 <mfs132@hotmail.com>
2024-11-23 15:39:33 -0300

    conserta?

--- src/commands/addnotas.js
+++ src/commands/addnotas.js
@@ -1,27 +1,44 @@
 const { SlashCommandBuilder } = require("discord.js");
 const fs = require("node:fs");
 
+//estrutura "conteudo" = {user: [{titulo: "", texto: ""}]}
 let conteudo = {
-    rotinoM: [{titulo: "texto"},]
 };
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("addnota")
         .setDescription("permite criar e gerenciar notas diversas")
+        .addStringOption(titulo => 
+            titulo.setName("titulo")
+            .setDescription("titulo da nota")
+            .setRequired(true)
+        )
         .addStringOption(texto => 
             texto.setName("texto")
-            .setDescription("texto para anotar")),
+            .setDescription("texto para anotar"))
+            .setRequired(true),
     async execute(interaction)
     {
-        conteudo[interaction.user.displayName] += interaction.options.getString("texto");
+        const userName = interaction.user.displayName;
+        if(!conteudo[userName])
+        {
+            conteudo[userName] = [];
+        }
+        conteudo[userName].push({
+            titulo: interaction.options.getString("titulo", true), 
+
+            texto: interaction.options.getString("texto", false)
+    });
+        
         const json = JSON.stringify(conteudo);
         await interaction.reply("WIP");
+        console.log(conteudo)
         fs.writeFile("./textos.json", json, err => {
             if (err)
                 console.error(err);
             else{
-                console.log("arquivo salvo");
+                console.log("arquivo exportado");
             }
         });
     }
