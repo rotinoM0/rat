dcc61386346b5f300154fed4b7041920040433a4
rotinoM0 <mfs132@hotmail.com>
2025-05-30 12:07:13 -0300

    limpar arquivos

--- discloud/import/1748038564842/src/events/ready.js
+++ discloud/import/1748038564842/src/events/ready.js
@@ -1,9 +1,13 @@
-const { Events } = require("discord.js");
+const { Events, ActivityType } = require("discord.js");
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
         console.log(`Logado como: ${client.user.tag}`);
+        client.user.setActivity({
+            name: "chorar é ação livre.",
+            type: ActivityType.Playing
+        })
     },
 };
